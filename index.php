03f17caf4e85bad87c9891de5b270458ff97eaa5
Brian Bischoff <bbischoff@captechventures.com>
2017-04-09 08:27:16 -0400

    added menu

--- index.php
+++ index.php
@@ -9,19 +9,13 @@ use F3\Service\WorkoutService;
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>F3RVA</title>
+<? include __ROOT__ . '/include/head.php';?>
 
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-
-</head>
 <body>
+<? include __ROOT__ . '/include/nav.php';?>
 
-<?php
+<?
     $workoutService = new WorkoutService();
     $workouts = $workoutService->getWorkouts();
 ?>

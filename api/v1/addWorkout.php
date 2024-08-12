9560e9b4da70f334042702cb5c2795e4367d8beb
Brian Bischoff <bbischoff78@gmail.com>
2024-08-12 10:10:25 -0400

    return workoutid after add

--- api/v1/addWorkout.php
+++ api/v1/addWorkout.php
@@ -37,5 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	
 	$workoutService = new WorkoutService();
 	$workout = $workoutService->addWorkout($data);
+
+    echo '{ "id": ' . $workout . ' }';
 }
 ?>

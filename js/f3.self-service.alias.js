145832e9f1cea0d0c3f5a68760fd57db87b8f927
Brian Bischoff <bbischoff78@gmail.com>
2024-09-24 16:47:26 -0400

    made datatables consistent and added jquery functions to autopopulate alias requests

--- js/f3.self-service.alias.js
+++ js/f3.self-service.alias.js
@@ -1,12 +1,35 @@
 $(document).ready(function() {
-	$('#members').DataTable( {
+	$('#primary').DataTable( {
 		"order": [[ 1, "asc" ]],
         "paging": false,
-        "scrollY": 500,
+        "scrollY": 650,
         "searching": true,
-		"aoColumns": [
-            { "orderSequence": [ "desc", "asc" ] },
-            { "orderSequence": [ "desc", "asc" ] },
+		"columns": [
+            { orderable: false },
+            null
         ]
     } );
+
+	$('#alias').DataTable( {
+		"order": [[ 1, "asc" ]],
+        "paging": false,
+        "scrollY": 650,
+        "searching": true,
+		"columns": [
+            { orderable: false },
+            null
+        ]
+    } );
+
+    // when the primary radio button is clicked, set the memberId value to the selected member id
+    $('input[name="primary"]').on('click', function() {
+        var selectedValue = $(this).val();
+        $('#primaryMemberId').val(selectedValue);
+    });
+      
+    // when the alias radio button is clicked, set the associatedMemberId value to the selected member id
+    $('input[name="alias"]').on('click', function() {
+        var selectedValue = $(this).val();
+        $('#aliasMemberId').val(selectedValue);
+    });
 });
